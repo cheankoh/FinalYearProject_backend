8409e6c54410d2b1206da151d7379fc4b961c046
cheankoh <cheankoh1011@gmail.com>
2022-04-01 15:04:33 +0800

    Temporary template

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,4 +1,4 @@
 //Connection URL to connect to the mongodb atlas server
 module.exports = {
-    url: "mongodb+srv://admin:admin@cluster0.c19gq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-  };
+  url: "mongodb+srv://cheankoh:cheankoh@cluster0.vchf2.mongodb.net/blockchain_fyp?retryWrites=true&w=majority"
+};
